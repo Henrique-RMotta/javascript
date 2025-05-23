42390b9a422ba9bab084b1c5c54b2e89d60e6edd
H.Motta <henrique2008790@gmail.com>
2025-05-23 15:59:27 -0300

    abc

--- Javascript/pizzaria/pizzaria.js
+++ Javascript/pizzaria/pizzaria.js
@@ -1,3 +1,40 @@
+function exibirMensagem(texto,tipo){
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = texto;
+    //adciona a classe do estilo (sucesso ou erro)
+    mensagem.className = `mensagem ${tipo}`;
+    mensagem.classList.remove("hidden");
+
+    // Remove a mensagem após 3 segundos
+    setTimeout(() => {
+        mensagem.classList.add("hidden")
+    }, 3000); 
+}
+
+function validarlogin(){
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    //usuario e senha fixos para validação
+    // (você pode substituir por algo mais avançado)
+
+    const usuarioCorreto = "admin";
+    const senhaCorreta = "1234";
+
+    if(usuario === usuarioCorreto && senha === senhaCorreta){
+        exibirMensagem("Login realizado com sucesso!", "sucesso");
+        setTimeout(() => {
+            // Redireciona para a página principal
+            window.location.href = "index.html";
+        },1000);
+    } else {
+        exibirMensagem("Usuário ou senha incorretos", "erro");
+    }
+}
+
+
+
+
 let pizzas = [];
 let pizzasalterar = null;
 function mostrarsecao(secao){
@@ -5,6 +42,7 @@ function mostrarsecao(secao){
     document.getElementById("cadastrar").classList.add("hidden");
     document.getElementById("cardapio").classList.add("hidden");
     document.getElementById("alterar").classList.add("hidden");
+    document.getElementById("msgerro").classList.add("hidden");
     // mostra a seção de selecionada
     document.getElementById(secao).classList.remove("hidden");
 }
@@ -23,13 +61,22 @@ function cadastrarpizza() {
         document.getElementById("valor").value = "";
         document.getElementById("descricao").value = "";
         cardapio();
-        document.getElementById("msgerro").innerHTML = "Pizza cadastrada com sucesso"
+        document.getElementById("msgerro").innerHTML = `Pizza cadastrada com sucesso`
+        document.getElementById("msgerro").classList.remove("hidden");
     } else {
-        document.getElementById("msgerro").innerHTML = "Preencha todos os campos, ou houve um erro tente novamente mais tarde "
+        document.getElementById("msgerro").innerHTML = `Preencha todos os campos, ou houve um erro tente novamente mais tarde`
+        document.getElementById("msgerro").classList.remove("hidden");
     }
+   
 
 }
-
+function buscarpizza() {
+    const busca = document.getElementById("busca").value.toLowerCase();
+    const resultados = pizzas.filter((pizza) => 
+     pizza.nomepizza.toLowerCase().includes(busca)
+);
+    cardapio(resultados);
+}
 function cardapio(lista = pizzas){
     const tabela = document.getElementById("lista-pizzas");
     tabela.innerHTML = "";
